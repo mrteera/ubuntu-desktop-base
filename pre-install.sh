a63ed1e1ccf3ed2324b825cd9e26419cea886a13
mrteera <webmaster@mrteera.com>
2014-01-21 20:29:17 +0700

    update Chef installation

--- pre-install.sh
+++ pre-install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get dist-upgrade -y
-sudo apt-get autoremove -y
-sudo apt-get install curl -y
+sudo apt-get update && sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+# Install Chef
+sudo apt-get -y install curl build-essential libxml2-dev libxslt-dev git
 curl -L https://www.opscode.com/chef/install.sh | sudo bash
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+
+# Install Berkshelf with chef's own ruby
+sudo /opt/chef/embedded/bin/gem install berkshelf
